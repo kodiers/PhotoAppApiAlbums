d6bf1c6c1f721172fde4669f17ef35b3af9be1c7
Victor Yamchinov <kodiers@gmail.com>
2022-03-31 23:35:43 +0300

    Initial commit

---
+++ src/main/java/com/tfl/photoappapialbums/PhotoAppApiAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.tfl.photoappapialbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiAlbumsApplication.class, args);
+    }
+
+}
